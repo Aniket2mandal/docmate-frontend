1f70fe7e39ae87adfee6de018446fca4f46f1687
Aniket Mandal <104608457+Aniket2mandal@users.noreply.github.com>
2026-07-13 11:18:59 +0545

    fixed link

--- docmate-app/src/pages/Register/Register.jsx
+++ docmate-app/src/pages/Register/Register.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate,Link } from "react-router-dom";
 import "./Register.css";
 import { registerUser } from "../../api/BackendApi";
 import Swal from "sweetalert2";
